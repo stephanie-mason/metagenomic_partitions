28a456d69797ee71a0aedffb7a3b1deffee1143a
Stephanie Mason <Stephanie Mason>
2017-03-05 21:19:04 -0800

    making progress

--- toJson.py
+++ toJson.py
@@ -24,15 +24,51 @@ outputFile = inputFile + ".json"
 outputFile = open(outputFile, 'w')
 
 # Nested Dictionaries
-dataDict = {}
+dataDict = {"willBeFuzzy": []}
 
 with open(inputFile + ".csv", newline='') as tsvFile:
     fileReader = csv.DictReader(tsvFile, delimiter='\t')
+    currDivision = None
+    currDivDict = None
     for row in fileReader:
-        currPlacementType = row["placement_type"]
-        currDate = row["date"]
-        dataDict[currPlacementType] = currDate
+        prevDivision = currDivision
 
-print(dataDict)
+        currPlacement = row["placement_type"]
+        currDate = row["date"]
+        currDepth = row["sample_depth"]
+        currDivision = row["division_name"]
+        currClass = row["lowest_classification_name"]
+        currCount = row["RR_count"]
+
+        # what happens when you get a new division.. lets see, you need to
+        # do something with the dictionary you have... you need to append it! here!
+        # then
+        # create a new dictionary
+        # with the "name": = currDivision
+        # children = []
+        if currDivision != prevDivision:
+            if currDivDict != None:
+                dataDict["willBeFuzzy"].append(currDivDict)
+            currDivDict = {"name": currDivision, "children": []}
+
+        currDivDict["children"].append({"name": currClass, "size": currCount})
+
+
+
+
+
+#json_string = json.dumps(dataDict, indent=4)
+json_string = json.dumps(dataDict, indent=4)
+print(json_string)
+outputFile.write(json_string)
 
 outputFile.close()
+
+# Key                           # value(s)
+# confidence level              date dictionary
+# date                          sample_depth dictionary
+# sample depth                  division dictionary
+# division                      name, lowest classification dictionary
+# lowest classification         name of gene, size
+
+#row["sample_depth"]} for row in fileReader
