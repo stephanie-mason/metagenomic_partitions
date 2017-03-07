cc59f88ae3ba79914cab5e015cb6029dd2a7b88b
Stephanie Mason <Stephanie Mason>
2017-03-07 12:09:43 -0800

    so close to having it output data in the right way

--- toJson.py
+++ toJson.py
@@ -2,12 +2,6 @@
 # This is a simple script to turn some TSV data into JSON for purposes of
 # creating a bilevel partition in D3.
 #
-# It writes a JSON file with a hierarchy given in the arguments
-#
-# IE Arg1 will have children Arg2, which will have children Arg 3
-#
-# Output is a json file with the same name as the input file
-#
 # Author: Stephanie Mason
 #==============================================================================#
 import sys, csv, json
@@ -19,72 +13,88 @@ import sys, csv, json
 # Input Variables
 inputFile = sys.argv[1]
 
-# Output Variables
-outputFile = inputFile + ".json"
-outputFile = open(outputFile, 'w')
+# New tactic: First build a set of dictionaries from the data,
+# iterating through it once
+# One for Date, one for Depth, one for Placement Type
+# This dictionary will have the keys that are values from the table
+# THEN !!!!
+# Make new output dictionaries that have "name" keys and list values from
+# your handy, descriptive key dictionaries
+# Dictionaries everywhere!!!
 
-# Nested Dictionaries
-dataDict = {"name": "dates", "children": []}
+# Dictionaries
+# First Level Dicts
+placementDict = {}
+dateDict = {}
+depthDict = {}
+
+# Second Level Dict
+#divisionDict = {}
+
+# Third Level Dict
+#classDict = {}
 
 with open(inputFile + ".csv", newline='') as tsvFile:
     fileReader = csv.DictReader(tsvFile, delimiter='\t')
-    currPlacement = None
-    currDate = None
-    currDepth = None
-    currDivision = None
-
-    currPlaceDict = None
-    currDateDict = None
-    currDepthDict = None
-    currDivDict = None
     for row in fileReader:
-        prevPlacement = currPlacement
-        prevDate = currDate
-        prevDepth = currDepth
-        prevDivision = currDivision
-
         currPlacement = row["placement_type"]
-        currDate = row["date"]
-        currDepth = row["sample_depth"]
+        #currDate = row["date"]
+        #currDepth = row["sample_depth"]
         currDivision = row["division_name"]
         currClass = row["lowest_classification_name"]
-        currCount = row["RR_count"]
+        currCount = int(row["RR_count"])
 
-        # New Placement Type
-        #if currPlacement != prevPlacement:
-        #    currPlaceDict = {"name": currPlacement, "children": []}
-        #    dataDict["children"].append(currPlaceDict)
+        if currPlacement not in placementDict:
+            placementDict[currPlacement] = {}
 
-        # New Date
-        if currDate != prevDate:
-            currDateDict = {"name": currDate, "children": []}
-            dataDict["children"].append(currDateDict)
+        if currDivision not in placementDict[currPlacement]:
+            placementDict[currPlacement][currDivision] = {}
 
-        # New depth in Date
+        if currClass not in placementDict[currPlacement][currDivision]:
+            placementDict[currPlacement][currDivision][currClass] = currCount
+        else:
+            placementDict[currPlacement][currDivision][currClass] += currCount
 
-        # New Division
-        if currDivision != prevDivision:
-            if currDivDict != None:
-                currDateDict["children"].append(currDivDict)
-            currDivDict = {"name": currDivision, "children": []}
+        #placementDict[currPlacement] = divisionDict
+        #divisionDict[currDivision] = classDict
 
-        currDivDict["children"].append({"name": currClass, "size": currCount})
-
+        #if currClass in classDict:
+        #    placementDict[currPlacement][currDivision][currClass] += currCount
+        #else:
+        #    placementDict[currPlacement][currDivision][currClass] = currCount
 
 
+print(placementDict["fuzzy"]["Alveolata"]["Dinophyceae"])
 
-
-json_string = json.dumps(dataDict, indent=2)
-print(json_string)
+json_string = json.dumps(placementDict, indent=2)
+outputFile = "all_data.json"
+outputFile = open(outputFile, 'w')
 outputFile.write(json_string)
-
 outputFile.close()
 
-# Key                           # value(s)
-# confidence level              date dictionary
-# date                          sample_depth dictionary
-# sample depth                  division dictionary
-# division                      name, lowest classification dictionary
-# lowest classification         name of gene, size
+# Write output dictionaries
+placementFinalDict = {"name": "placements", "children": []}
 
-#row["sample_depth"]} for row in fileReader
+iters = 0;
+for placement in placementDict:
+    placementFinalDict["children"].append({"name": placement, "children": []})
+    for division in placementDict[placement]:
+        inneriters=0
+        placementFinalDict["children"][iters]["children"].append({"name": division, "children": []})
+        for className in placementDict[placement][division]:
+            placementFinalDict["children"][iters]["children"][inneriters]["children"].append({"name": className, "size": placementDict[placement][division][className]})
+        inneriters += 1
+    iters += 1
+
+finalDict = placementFinalDict
+
+json_string = json.dumps(finalDict, indent=2)
+#print(json_string)
+
+# Outputs
+outputFiles = ["placement_sorted.json", "date_sorted.json", "depth_sorted.json"]
+
+for outputFile in outputFiles:
+    outputFile = open(outputFile, 'w')
+    outputFile.write(json_string)
+    outputFile.close()
